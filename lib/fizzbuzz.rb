00cec74ff925bf5d116da5db7d1a999a7a7c5294
Sadat <sadatmanjur15@gmail.com>
2022-08-11 17:43:02 +0000

    fizzbuzz completed

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    return "fizzbuzz"
+  end
+  
+  if number % 3 == 0
+    return "fizz"
+  end 
+  
+  if number % 5 == 0
+    return "buzz"
+  end
+
+  return number  
+  
+end
